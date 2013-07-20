db34b7d341cb45672f6db3fcae4855bdc8557f79
Basil Safwat <bsafwat@gmail.com>
2013-07-20 10:59:46 +0100

    Add expand list item animation

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.block-list-add-item').click(function() {
+    $('<li class="block-list-item block-list-new-item">Item</li>').insertBefore('li:first-child');
+  });
+});
